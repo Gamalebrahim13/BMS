a7bd31caf37370070176ca35482ad5be96db27bb
mohamedyasser123 <104241460+mohamedyasser123@users.noreply.github.com>
2026-05-13 15:49:05 +0300

    Register is done

--- src/api/axiosClient.tsx
+++ src/api/axiosClient.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "upskilling-egypt.com:3003/api/v1", // غيرها بالـ API بتاعك
+  baseURL: "https://upskilling-egypt.com:3003/api/v1",
   timeout: 5000,
 });
 
@@ -17,12 +17,17 @@ axiosClient.interceptors.request.use((config) => {
 });
 
 // Response Interceptor
-axiosClient.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    console.log("Error:", error.response?.data || error.message);
-    return Promise.reject(error);
+axiosClient.interceptors.response.use( (response) => {
+    return response;
+  },(error)=>{
+     if (error.response && error.response.status === 401){
+        localStorage.clear();
+       window.location.href = '/login';
+
+     }
+     return Promise.reject(error);
   }
-);
+
+)
 
 export default axiosClient;
