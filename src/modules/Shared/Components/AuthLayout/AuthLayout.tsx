9c3d3b813a5c9dbfed55f527fc7e3c422e7afdbd
mohamedyasser123 <104241460+mohamedyasser123@users.noreply.github.com>
2026-05-14 21:46:29 +0300

    background is done

--- src/modules/Shared/Components/AuthLayout/AuthLayout.tsx
+++ src/modules/Shared/Components/AuthLayout/AuthLayout.tsx
@@ -1,13 +1,28 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import bgAuth from "../../../../assets/images/bgAuth.png";
 import logo from "../../../../assets/images/logo.png";
+import loginBg from "../../../../assets/images/login-bg.jpeg"
+import forgetBg from "../../../../assets/images/forget-pass-bg.jpeg"
+import restBg from "../../../../assets/images/rest-password-bg.jpeg"
+import changBg from "../../../../assets/images/change-password-bg.jpeg"
 
 export default function AuthLayout() {
+   const location = useLocation();
+     const backgrounds: Record<string, string> = {
+    "/login": loginBg,
+    "/register": restBg,
+    "/verify-account": changBg,
+    "/change-password": changBg,
+    "/forget-password": forgetBg,
+    "/reset-password": restBg,
+  };
+   const currentBg =
+    backgrounds[location.pathname] || loginBg;
   return (
     <div
       className="min-h-screen bg-cover bg-center flex items-center justify-center px-4"
       style={{
-        backgroundImage: `url(${bgAuth})`,
+        backgroundImage: `url(${currentBg})`,
       }}
     >
       <div className="w-full max-w-md">
