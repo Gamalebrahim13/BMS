e599880b3d1ef7d03596b14596eb263d158c4cbe
Moaz Taha <moaztaha62@gmail.com>
2026-05-14 23:01:58 +0300

    added import type in login Page

--- src/modules/Authentication/components/Login/Login.tsx
+++ src/modules/Authentication/components/Login/Login.tsx
@@ -2,14 +2,13 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useContext, useState } from "react";
-import { IoEyeOffOutline } from "react-icons/io5";
-import { IoEyeOutline } from "react-icons/io5";
 import { login } from "../../../../api/module/auth";
 import { toast } from "react-toastify";
 import { AuthContext } from "../../../../context/AuthContext";
+import type { LoginData } from "../../../../api/module/auth";
+import Input from "../../../Shared/Components/custominput";
 export default function Login() {
   const [loading, setLoading] = useState(false);
-  const [showPassword, setShowPassword] = useState(false);
   const context = useContext(AuthContext);
   const navigate = useNavigate();
 
@@ -18,18 +17,14 @@ export default function Login() {
   }
 
   const { saveLoginData } = context;
-  type LoginFormData = {
-    email: string;
-    password: string;
-  };
 
   let {
     register,
     formState: { errors },
     handleSubmit,
-  } = useForm<LoginFormData>();
+  } = useForm<LoginData>();
 
-  const onSubmit = async (data: LoginFormData) => {
+  const onSubmit = async (data: LoginData) => {
     if (loading) return;
 
     setLoading(true);
@@ -41,7 +36,19 @@ export default function Login() {
       navigate("/register");
       toast.success("Login Successfully");
     } catch (error: any) {
-      toast.error(error?.response?.data?.message);
+      const errors = error?.response?.data?.additionalInfo?.errors;
+
+      if (errors) {
+        Object.values(errors).forEach((messages: any) => {
+          if (Array.isArray(messages)) {
+            messages.forEach((msg: string) => {
+              toast.error(msg);
+            });
+          }
+        });
+      } else {
+        toast.error(error?.response?.data?.message || "Something went wrong");
+      }
     } finally {
       setLoading(false);
     }
@@ -56,53 +63,27 @@ export default function Login() {
         </h3>
       </div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex flex-col mb-2 ">
-          <label className="text-primary text-sm font-light mb-0.5 ">
-            E-mail
-          </label>
-          <input
-            placeholder="Enter your E-mail"
-            {...register("email", {
-              required: "field is required",
-              pattern: {
-                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                message: "Email is not valid",
-              },
-            })}
-            type="text"
-            className="bg-transparent font-light outline-none text-white placeholder:text-white placeholder:text-md w-full "
-          />
-        </div>
-        <hr className="border-white/20" />
-        {errors.email && (
-          <span className="text-primary">{errors.email.message}</span>
-        )}
+        <Input
+          label="Email"
+          type="email"
+          required
+          placeholder="Enter email"
+          register={register("email", {
+            required: "Email is required",
+          })}
+          error={errors.email}
+        />
 
-        <div className="flex flex-col mb-2 mt-5 relative ">
-          <label className="text-primary text-sm font-light mb-0.5  ">
-            Password
-          </label>
-          <div className="relative flex items-center">
-            <input
-              {...register("password", {
-                required: "field is required",
-              })}
-              type={showPassword ? "text" : "password"}
-              placeholder="Enter your password"
-              className="bg-transparent font-light outline-none text-white placeholder:text-white placeholder:text-md w-full "
-            />
-
-            <span
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute right-0 cursor-pointer text-white text-md ">
-              {showPassword ? <IoEyeOffOutline /> : <IoEyeOutline />}
-            </span>
-          </div>
-        </div>
-        <hr className=" border-white/20 " />
-        {errors.password && (
-          <span className="text-primary">{errors.password.message}</span>
-        )}
+        <Input
+          label="Password"
+          type="password"
+          required
+          placeholder="Enter password"
+          register={register("password", {
+            required: "Password is required",
+          })}
+          error={errors.password}
+        />
 
         <div className="links mt-3 text-white font-light flex justify-between text-sm">
           <Link to="/register"> Register Now ?</Link>
