d8dbb22b78692a738c08f19c7522bfca1854e0bc
Moaz Taha <moaztaha62@gmail.com>
2026-05-19 18:59:34 +0300

    add no data page

--- src/modules/Shared/Components/NoData/NotData.tsx
+++ src/modules/Shared/Components/NoData/NotData.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
+import nodataImg from "../../../../assets/images/no-data.png";
 
-export default function NotData() {
+interface NoDataProps {
+  title?: string;
+  subTitle?: string;
+  showButton?: boolean;
+  buttonText?: string;
+  navigateTo?: string;
+}
+
+export default function NoData({
+
+}: NoDataProps) {
+
   return (
-    <div>NotData</div>
-  )
+    <div className="flex flex-col items-center justify-center py-10 text-center">
+      {/* IMAGE */}
+      <img
+        src={nodataImg}
+        alt="No Data"
+        className="w-32 h-32 object-contain opacity-80"
+      />
+      <h3 className="mt-3 text-lg font-semibold text-gray-700">No Data Found</h3>
+    </div>
+  );
 }
